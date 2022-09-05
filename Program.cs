48b51ac00ae3c5553b8715634daa28caadd0f3fc
Viktoria <kirvikanik@gmail.com>
2022-09-05 18:19:00 +0300

    program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿string[] Array = {"hello", "2", "world", ":-)" };
+string str = string.Join(", ", Array);
+Console.Write($"[{str}] -> ");
+
+for (int i = 0; i < Array.Length; i++)
+
+{
+    if (Array[i].Length <= 3)
+    {
+        if (i != Array.Length - 1)
+        {
+            Console.Write($"{Array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{Array[i]}");
+        }
+    }
+}
